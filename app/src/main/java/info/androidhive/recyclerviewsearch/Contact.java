a756c50f77f7bd01bde55656cff2bb584e768ff1
BSH <lazy_hyun@naver.com>
2018-11-02 21:20:22 +0900

    first commit

---
+++ app/src/main/java/info/androidhive/recyclerviewsearch/Contact.java
@@ -0,0 +1,26 @@
+package info.androidhive.recyclerviewsearch;
+
+/**
+ * Created by ravi on 16/11/17.
+ */
+
+public class Contact {
+    String koreanName;
+    String image;
+    String englishName;
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return koreanName;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return englishName;
+    }
+}
